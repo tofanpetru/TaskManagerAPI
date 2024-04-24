b4da2ff062e1911e171672874c91f593f2297ef0
Tofan Petru <tofanpetru12@gmail.com>
2024-04-25 01:30:11 +0300

    add dbcontext

---
+++ src/2.Services/Task.Manager/Task.Manager.Service/Domain/DataConnections/ITaskManagerContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Task.Manager.Service.Domain.DataConnections;
+
+internal interface ITaskManagerContext
+{
+    DbSet<Contracts.Commons.Entities.Task> Tasks { get; set; }
+    DbSet<T> Set<T>() where T : class;
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ src/2.Services/Task.Manager/Task.Manager.Service/Infrastructure/Data/TaskManagerContext.cs
@@ -0,0 +1,28 @@
+﻿using Core.Configurations;
+using Core.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Task.Manager.Service.Domain.DataConnections;
+
+namespace Task.Manager.Service.Infrastructure.Data;
+
+internal sealed class TaskManagerContext : DbContext, ITaskManagerContext
+{
+    public TaskManagerContext(DbContextOptions<TaskManagerContext> options) : base(options) { }
+
+    public DbSet<Contracts.Commons.Entities.Task> Tasks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(BaseDataModelConfiguration).Assembly);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TaskManagerContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        this.ApplyTrackingChanges();
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+}
