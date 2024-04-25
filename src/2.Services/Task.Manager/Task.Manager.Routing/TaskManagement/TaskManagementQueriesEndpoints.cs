62911f031583f30e237a9127ae58e7ce968d3647
Tofan Petru <tofanpetru12@gmail.com>
2024-04-25 23:35:46 +0300

    register get task by id endpoint

--- src/2.Services/Task.Manager/Task.Manager.Routing/TaskManagement/TaskManagementQueriesEndpoints.cs
+++ src/2.Services/Task.Manager/Task.Manager.Routing/TaskManagement/TaskManagementQueriesEndpoints.cs
@@ -1,7 +1,12 @@
 ﻿using Core.Endpoints;
+using Core.Result;
+using FluentResults;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
+using Task.Manager.Contracts.TaskManagement.Queries;
 using ApiVersion = Asp.Versioning.ApiVersion;
 
 namespace Task.Manager.Routing.TaskManagement;
@@ -19,5 +24,22 @@ internal sealed class TaskManagementQueriesEndpoints : IEndpointsDefinition
             .WithOpenApi()
             .WithValidationFilter()
             .WithApiVersionSet(versionSet);
+
+        group.MapGet("/{id:guid}", GetTaskByIdAsync)
+             .Produces<Result<Contracts.Commons.Entities.Task>>(StatusCodes.Status200OK)
+             .ProducesProblem(StatusCodes.Status404NotFound)
+             .WithName(nameof(GetTaskByIdAsync))
+             .WithSummary("");
+    }
+
+    private static async Task<IResult> GetTaskByIdAsync(
+        [FromRoute] Guid id,
+        IMediator mediator)
+    {
+        var operation = await mediator.Send(new GetTaskByIdQuery(id));
+
+        return operation.IsSuccess
+            ? Results.Ok(operation.Value)
+            : operation.ToProblemDetails();
     }
 }
